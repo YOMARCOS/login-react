975ffeb9bd1d9c01950715b8eaa5a72006944016
YOMARCOS <marckos0011@hotmail.com>
2023-08-10 22:56:09 -0300

    Updates

---
+++ precache-manifest.39fede6194699075032cbabe6c9b30c1.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c0a207a338505a21d0023885e107e41",
+    "url": "%7Busername%7D.github.io/%7Brepo-name%7D/index.html"
+  },
+  {
+    "revision": "57deaf3b7d720215c8a1",
+    "url": "%7Busername%7D.github.io/%7Brepo-name%7D/static/css/main.6b01ac82.chunk.css"
+  },
+  {
+    "revision": "74365ca89aa2df6b6f23",
+    "url": "%7Busername%7D.github.io/%7Brepo-name%7D/static/js/2.25303303.chunk.js"
+  },
+  {
+    "revision": "57deaf3b7d720215c8a1",
+    "url": "%7Busername%7D.github.io/%7Brepo-name%7D/static/js/main.efb0bc4e.chunk.js"
+  },
+  {
+    "revision": "08a6a57dd0ebbf8e9c11",
+    "url": "%7Busername%7D.github.io/%7Brepo-name%7D/static/js/runtime~main.1882f72f.js"
+  },
+  {
+    "revision": "5a798cdadc7cd321e3f72425b70bface",
+    "url": "%7Busername%7D.github.io/%7Brepo-name%7D/static/media/OpenSans-Regular.5a798cda.ttf"
+  },
+  {
+    "revision": "f65d1a07e0f4521c99d900e31e4bc530",
+    "url": "%7Busername%7D.github.io/%7Brepo-name%7D/static/media/Quicksand-Medium.f65d1a07.ttf"
+  },
+  {
+    "revision": "e3f2d3ab564cba2a32d40dac569ddf95",
+    "url": "%7Busername%7D.github.io/%7Brepo-name%7D/static/media/icon-locker.e3f2d3ab.svg"
+  },
+  {
+    "revision": "ced06be7acb508ddf0eeb88ba478bd33",
+    "url": "%7Busername%7D.github.io/%7Brepo-name%7D/static/media/icon-user.ced06be7.svg"
+  }
+]);
